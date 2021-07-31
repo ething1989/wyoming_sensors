6032b0bbdd88bf19369159a86e1fc109c2323a97
ething1989 <82768028+ething1989@users.noreply.github.com>
2021-07-31 08:31:40 -0600

    Update wyoming_sensors_actions_update.py

--- wyoming_sensors_actions_update.py
+++ wyoming_sensors_actions_update.py
@@ -1,11 +1,12 @@
 ##Keep this first part to continue logging whatever you want by appending onto the existing logfile.txt
 
-import time
+import datetime
 
-timestamp1 = time.strftime("%Y-%m-%d  %H:%M:%S")
+current_date_and_time = datetime.datetime.now()
+current_date_and_time_string = str(current_date_and_time)
 
 f= open("/home/pi/logs/logfile.txt","a+")
-f.write("/n Hello idiots, it is " time.strftime("%Y-%m-%d  %H:%M:%S"))
+f.write("/n Hello idiots, it is " current_date_and_time_string)
 f.close()
 
 ##Add scripts to be run below here.

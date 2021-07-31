4e4157d985db1f40e0b2563f9bec0a72ab566806
ething1989 <82768028+ething1989@users.noreply.github.com>
2021-07-31 08:36:04 -0600

    Update wyoming_sensors_actions_update.py

--- wyoming_sensors_actions_update.py
+++ wyoming_sensors_actions_update.py
@@ -6,7 +6,7 @@ current_date_and_time = datetime.datetime.now()
 current_date_and_time_string = str(current_date_and_time)
 
 f= open("/home/pi/logs/logfile.txt","a+")
-f.write("/n Hello idiots, it is " % (current_date_and_time_string))
+f.write(% (current_date_and_time_string))
 f.close()
 
 ##Add scripts to be run below here.

c34f53670b6db53900a95fc0d694ef7973b438a7
ething1989 <82768028+ething1989@users.noreply.github.com>
2021-07-31 08:24:28 -0600

    Update wyoming_sensors_actions_update.py

--- wyoming_sensors_actions_update.py
+++ wyoming_sensors_actions_update.py
@@ -1,9 +1,11 @@
 ##Keep this first part to continue logging whatever you want by appending onto the existing logfile.txt
 
-DATE=$(date --date="${dataset_date} -${date_diff} 1 day" +%Y-%m-%d)
+import time
+
+timestamp1 = time.strftime("%Y-%m-%d  %H:%M:%S")
 
 f= open("/home/pi/logs/logfile.txt","a+")
-f.write("Hello idiots, it is ", $DATE, " \n")
+f.write("Hello idiots, it is ", timestamp1, " \n")
 f.close()
 
 ##Add scripts to be run below here.

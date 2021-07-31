a346f5843c3554de02708dc48f0bf47a0cf38798
ething1989 <82768028+ething1989@users.noreply.github.com>
2021-07-31 08:19:57 -0600

    Update wyoming_sensors_actions_update.py

--- wyoming_sensors_actions_update.py
+++ wyoming_sensors_actions_update.py
@@ -1,7 +1,9 @@
 ##Keep this first part to continue logging whatever you want by appending onto the existing logfile.txt
 
+DATE=$(date --date="${dataset_date} -${date_diff} 1 day" +%Y-%m-%d)
+
 f= open("/home/pi/logs/logfile.txt","a+")
-f.write("Hello idiots \n")
+f.write("Hello idiots, it is ", $DATE, " \n")
 f.close()
 
 ##Add scripts to be run below here.

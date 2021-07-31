eb58515096f544f6fde93bba8785233e0fa35e0e
ething1989 <82768028+ething1989@users.noreply.github.com>
2021-07-31 07:58:44 -0600

    Update wyoming_sensors_actions_update.py

--- wyoming_sensors_actions_update.py
+++ wyoming_sensors_actions_update.py
@@ -1,3 +1,7 @@
+##Keep this first part to continue logging whatever you want by appending onto the existing logfile.txt
+
 f= open("logfile.txt","a+")
 f.write("Hello world")
 f.close()
+
+##Add scripts to be run below here.

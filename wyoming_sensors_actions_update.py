8da7b6e2819ed9e773549f69b108e37bae49bb0a
ething1989 <82768028+ething1989@users.noreply.github.com>
2021-08-02 16:24:38 -0400

    Update wyoming_sensors_actions_update.py

--- wyoming_sensors_actions_update.py
+++ wyoming_sensors_actions_update.py
@@ -2,12 +2,36 @@
 ##Add scripts to be run below here, and if testing sensors, have the output print into the log file so you know if they are working
 
 import datetime
+import os 
+
+
+def getDfDescription():
+    df = os.popen("df -h /")
+    i = 0
+    while True:
+        i = i + 1
+        line = df.readline()
+        if i==1:
+            return(line.split()[0:6])
+                                 
+def getDf():
+    df = os.popen("df -h /")
+    i = 0
+    while True:
+        i = i + 1
+        line = df.readline()
+        if i==2:
+            return(line.split()[0:6])
 
 current_date_and_time = datetime.datetime.now()
 current_date_and_time_string = str(current_date_and_time)
+description = getDfDescription()
+disk_root = getDf()
+space = disk_root[4]
 
 f= open("/home/pi/logs/logfile.txt","a+")
 f.write(current_date_and_time_string)
-f.write("  hello idiot  \r\n")
+f.write("  all good  \r\n")
+f.write(space)
 f.close()
 
